2614b9b5983b350ce0298357c157654eabc84721
hector3211 <hectororopesa5@gmail.com>
2025-04-24 19:44:24 -0400

    update

--- internal/codegen/gogen/select.go
+++ internal/codegen/gogen/select.go
@@ -1,1 +1,14 @@
 package gogen
+
+import (
+	"shogunc/internal/sqlparser"
+	"strings"
+)
+
+type GoSelectFuncGenerator struct {
+	b strings.Builder
+}
+
+func (g *GoSelectFuncGenerator) GenerateFunc(query *sqlparser.SelectStatement) string {
+	return ""
+}
